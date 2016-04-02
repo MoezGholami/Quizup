fd584fd92ba3078b0efacc5d1f544a39d3c145b3
mahyarABS <mahyar.abbasian@yahoo.com>
2016-04-02 16:26:53 +0430

    category index edit and new UI completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309175959) do
+ActiveRecord::Schema.define(version: 20160402091823) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "image"
   end
 
   create_table "questions", force: :cascade do |t|
